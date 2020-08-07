a4905a5a22696d35ba1bde2615b9484c30354fc3
Nathan Steed <steedn@outlook.com>
2020-08-07 04:38:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
   @@count
-  @@genres
+  @@genres = []
   @@artists
   def initialize(song, artist, genre)
     @@song_count += 1
+    @@genre << genre
   end
 
   def count
     @@count
   end
+
+def genres
+  
